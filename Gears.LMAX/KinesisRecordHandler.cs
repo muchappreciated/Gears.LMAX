1f6a2410863f72b6fa0f4aae4c45471293ce33eb
Steve Ireland <steve@automatagears.com>
2015-05-18 22:28:32 +0100

    Init commit

---
+++ Gears.LMAX/KinesisRecordHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Gears.LMAX
+{
+    using System;
+
+    using Amazon.Kinesis.Model;
+
+    using Disruptor;
+
+    public class KinesisRecordHandler : IEventHandler<Record>
+    {
+        public void OnNext(Record data, long sequence, bool endOfBatch)
+        {
+            Console.WriteLine("Handled event #{0}", sequence);
+        }
+    }
+}
---
+++ Gears.LMAX/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Gears.LMAX
+{
+    using Amazon.Kinesis.Model;
+
+    using Disruptor.Dsl;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int RingBufferSize = 1024;
+            var disruptor = new Disruptor<Record>(() => new Record(), RingBufferSize, TaskScheduler.Default);
+
+            disruptor.HandleEventsWith(new KinesisRecordHandler());
+
+            var ringBuffer = disruptor.Start();
+
+            while (true)
+            {
+
+                var batchedRecords = ringBuffer.NewBatchDescriptor(100);
+
+                ringBuffer.Next(batchedRecords);
+
+                ringBuffer.Publish(batchedRecords);
+            }
+        }
+    }
+}
